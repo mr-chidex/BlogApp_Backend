c47450d8dd050179b5735cd857ccdd180440d604
mr-chidex <mbahchiboy2@gmail.com>
2021-10-27 18:46:24 +0100

    news letter subscription endpoint added to frontend

--- backend/controllers/newsLetter.js
+++ backend/controllers/newsLetter.js
@@ -7,6 +7,7 @@ const NewsLetter = require("../models/newsLetter");
 //@Route    POST /api/user/news-letter
 //@access   Public
 exports.subscribe = async (req, res) => {
+  console.log(req.body);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({
--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,9 +3,37 @@ import { Link } from "react-router-dom";
 import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 import { Button, IconButton, TextField } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core";
+
+import { setSnackbar } from "../redux/actions/uiActions";
+
+const useStyles = makeStyles({
+  btn: { backgroundColor: "#eee" },
+});
 
 const SideBar = () => {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  const subscribeHandler = async () => {
+    try {
+      setLoading(true);
+      await axios.post(`${process.env.REACT_APP_BLOG_API}/api/news-letter`, {
+        email,
+      });
+      dispatch(setSnackbar("successfully subscribed", "success"));
+      setLoading(false);
+    } catch (error) {
+      error.response && error.response.data.message
+        ? dispatch(setSnackbar(error.response.data.message, "error"))
+        : dispatch(setSnackbar(error.message, "error"));
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="side-bar">
@@ -50,12 +78,14 @@ const SideBar = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
           <Button
-            className="btn"
             type="submit"
             variant="contained"
             color="primary"
             size="large"
             fullWidth
+            className={loading ? classes.btn : "btn"}
+            disabled={loading}
+            onClick={subscribeHandler}
           >
             Subscribe
           </Button>
